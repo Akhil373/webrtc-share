ac1b25f317f72022c197051832590096ee02c2be
Akhil373 <akhil2005.m@gmail.com>
2025-10-22 20:31:50 +0530

    http server

--- webrtc-signaling/ws-server.js
+++ webrtc-signaling/ws-server.js
@@ -1,10 +1,17 @@
 import "dotenv/config";
+import http from "http";
 import { WebSocketServer } from "ws";
 
 const port = process.env.PORT || 8080;
 const host = "0.0.0.0";
 
-export const wss = new WebSocketServer({ port, host });
-wss.on("listening", () => {
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("WebSocket signaling server is running\n");
+});
+
+export const wss = new WebSocketServer({ server });
+
+server.listen(port, host, () => {
     console.log(`WebSocket server listening on ${host}:${port}`);
 });
