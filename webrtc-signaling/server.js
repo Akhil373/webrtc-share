7877f9b5d0d31e3a6b331ed1f49cd4d25874a63e
Akhil373 <akhil2005.m@gmail.com>
2025-10-29 19:40:50 +0530

    test

--- webrtc-signaling/server.js
+++ webrtc-signaling/server.js
@@ -91,6 +91,10 @@ function fetchIP(msg) {
 wss.on("connection", function connection(ws, req) {
     ws.id = nanoid(5);
     const rawIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    if (rawIp && typeof rawIp === "string" && rawIp.includes(",")) {
+        rawIp = rawIp.split(",")[0].trim();
+    }
+
     ws.ip = rawIp.includes("::ffff:") ? rawIp.split(":").pop() : rawIp;
 
     console.log(ws.ip);
